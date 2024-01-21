1ac3073679267e516ee8cddc78a9fca19c7c2689
Jefferson de Oliveira Batista <jefferson.batista@xpi.com.br>
2024-01-21 19:42:33 -0300

    feat: Removendo classes não utlizadas

--- src/Infra/Database/models/AccountModel.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace Infra.Database.models;
-
-public class AccountModel
-{
-	[Key] [Required]
-	public string Id { get; set; } = string.Empty;
-}
